8b455c513eaecd94b53513aeb3b408af8f5de6bc
james russell <jamesrussell@pursuit.org>
2021-10-08 16:00:37 -0400

    accessing data

--- script.js
+++ script.js
@@ -15,7 +15,27 @@ form.addEventListener("submit", (e) => {
             return res.json()
         }).then((data) => {
             console.log(data)
-           
+            // display
+          let areaName = data.nearest_area[0].areaName[0].value
+          let regionName = data.nearest_area[0].region[0].value
+          let countryName = data.nearest_area[0].country[0].value
+          let feelsLike = data.current_condition[0].FeelsLikeF
+
+          // current and future forcast
+          let todaysAvg = data.weather[0].avgtempF
+          let todaysMaxTemp = data.weather[0].maxtempF
+          let todaysMinTemp = data.weather[0].mintempF
+
+          let tomorrowAvg = data.weather[1].avgtempF
+          let tomorrowMaxTemp = data.weather[1].maxtempF
+          let tomorrowMinTemp = data.weather[1].mintempF
+
+          let dayAfterAvg = data.weather[2].avgtempF
+          let dayAfterMaxTemp = data.weather[2].maxtempF
+          let dayAfterMinTemp = data.weather[2].mintempF
+
+         
+                                    
          
         }).catch((err) =>  {
     
